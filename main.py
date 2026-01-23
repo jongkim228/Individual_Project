b0ac36822476de1af5e066c31c6c0694761b398b
Jonghyun Kim <146230388+jongkim228@users.noreply.github.com>
2026-01-23 04:55:55 +0000

    box move implementation

--- main.py
+++ main.py
@@ -13,24 +13,21 @@ arm_actuator_names = [
 
 
 arm_actuator_ids = np.array([model.actuator(name).id for name in arm_actuator_names])
-
-
-actuator1 = model.actuator(arm_actuator_names[0]).id
-actuator2 = model.actuator(arm_actuator_names[1]).id
-actuator3 = model.actuator(arm_actuator_names[2]).id
-actuator4 = model.actuator(arm_actuator_names[3]).id
-actuator5 = model.actuator(arm_actuator_names[4]).id
-actuator6 = model.actuator(arm_actuator_names[5]).id
-gripper = model.actuator(arm_actuator_names[6]).id
+gripper_id = model.actuator("actuator8").id
 
 
 hand_id = model.body("hand").id
 hand_pos = data.xpos[hand_id]
 
 cube_id = model.body("cube1").id
+space_id = model.body("space").id
 cube_pos = data.xpos[cube_id]
+space_pos = data.xpos[space_id]
 
-def ik(model, data, hand_id, t_position):
+def smooth_move(current, target, speed=0.05):
+    return current + speed * (target - current)
+
+def ik(model, data, hand_id, t_position, alpha = 0.1):
 
     mujoco.mj_kinematics(model,data)
 
@@ -57,58 +54,74 @@ def ik(model, data, hand_id, t_position):
     jac_pos = np.zeros((3, model.nv))
     jac_rot = np.zeros((3, model.nv))
     point = data.xpos[hand_id]
-
     mujoco.mj_jac(model,data,jac_pos, jac_rot, point,hand_id)
-    jac = np.vstack([jac_pos, jac_rot])
+    J = np.vstack([jac_pos, jac_rot])
 
-    dq = np.linalg.pinv(jac) @ err
-    data.qvel[:7] = dq[:7]
+    JJt = J @ J.T
+    dq = J.T @ np.linalg.solve(JJt + 0.1 * np.eye(6), err)
 
+    q_current = data.qpos[:7].copy()
+    q_target = q_current + alpha * dq[:7]
+
+    data.ctrl[arm_actuator_ids] = q_target
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
 
     state = 'wait'
     start_time = data.time
-
     t_position = data.xpos[hand_id].copy()
+    goal_position = t_position.copy()
 
     while viewer.is_running():
         current_time = data.time - start_time
+        cube_pos = data.xpos[cube_id].copy()
         ik(model, data, hand_id, t_position)
         
         if state =='wait':
+            goal_position = data.xpos[hand_id].copy()
             if current_time > 2.0:
                 state = 'start'
 
         elif state == 'start':
-            t_position = cube_pos + np.array([0, 0, 0.5])
+            goal_position = cube_pos + np.array([0, 0, 0.5])
             if current_time > 4.0:
                 state = 'above'
 
         elif state == 'above':
-            data.ctrl[7] = 255
+            data.ctrl[gripper_id] = 255
             if current_time > 7.0:
                 state = 'opened'
-
+        
         elif state == 'opened':
-            t_position = cube_pos + np.array([0.01, 0, 0.17])
-            if current_time > 12.0:
+            goal_position = cube_pos + np.array([-0.18,0,0.24])
+            if current_time > 11.0:
                 state = 'down'
 
         elif state == 'down':
-            data.ctrl[7] = 0
+            data.ctrl[gripper_id] = 0
             if current_time > 14.0:
-                state = 'grab'
-                
-        elif state == 'grab':
-            data.ctrl[7] = 0
-            if current_time > 16.0:
-                t_position = cube_pos + np.array([0, 0, 0.5])
+                state = 'up'
+        
+        elif state =='up':
+            goal_position = cube_pos + np.array([-0.2,0,0.6])
+            if current_time > 15.0:
                 state = 'move'
-
+        
         elif state == 'move':
-            t_position =  np.array([0.5, 0.3, 0.5])
+            goal_position = space_pos + np.array([0,0,0.24])
+            if current_time > 17.0:
+                state = 'finish'
+        elif state == 'finish':
+            data.ctrl[gripper_id] = 255
+            goal_position = cube_pos + np.array([0, 0, 0.5])
+
+        
             
+
+        t_position = smooth_move(t_position, goal_position, speed=0.04)
+        
+
+        ik(model, data, hand_id, t_position, alpha=0.3)
 
         mujoco.mj_step(model, data)
         viewer.sync()
