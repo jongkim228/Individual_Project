fixing pick and place with z axis rotation

main.py
@@ -20,6 +20,8 @@ arm_actuator_names = [
 arm_actuator_ids = np.array([model.actuator(name).id for name in arm_actuator_names])
 gripper_id = model.actuator("actuator8").id
 
+
+
 #define values
 hand_id = model.body("hand").id
 hand_pos = data.xpos[hand_id]
@@ -175,23 +177,20 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
 
         if exceeds_length == False:
             t_rotation = d_rotation
-            inverse_kinematics(model,data, hand_id, t_position, t_rotation, arm_actuator_ids,exceeds_length)
         else:
             t_rotation = rotated
-            inverse_kinematics(model,data, hand_id, t_position, t_rotation, arm_actuator_ids,exceeds_length)
 
-        if state == "close_gripper":
-            data.ctrl[gripper_id] = 0 # 0
-        elif state == "open_gripper":
-            data.ctrl[gripper_id] = 255
+        inverse_kinematics(model,data, hand_id, t_position, t_rotation, arm_actuator_ids,exceeds_length)
+
 
         mujoco.mj_step(model, data)
+
 
         viewer.sync()
 
         renderer.update_scene(data,camera=camera_name)
         
-        #img = renderer.render()
+        img = renderer.render()
 
         renderer.enable_depth_rendering()
         depth = renderer.render()
@@ -201,7 +200,7 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
         
         calculate_in_local(model, data, camera_name,cube_id)
 
-        #cv2.imshow("Sub Camera", img[:, :, ::-1])
+        cv2.imshow("Sub Camera", img[:, :, ::-1])
 
         if cv2.waitKey(1) == 27:
             break

motions.py
@@ -9,26 +9,34 @@ def reached(ee_pos, goal_pos, tol):
     return np.linalg.norm(ee_pos - goal_pos) < tol
 
     
-def pick_and_place(model,data, exceeds_length,gripper_id, space_id,cube_id, ee_pos, state, state_start_time, gripper_open = 255, gripper_close = 0, tol = 0.07):
+def pick_and_place(model,data, exceeds_length,gripper_id, space_id,cube_id, ee_pos, state, state_start_time, tol = 0.07):
+
 
     start_pos_id = model.body("starting_space").id
 
     start_pos = data.xpos[start_pos_id]
     
     target_cube_pos = data.xpos[cube_id].copy()
+    near_cube = np.array([0.02, 0, 0.09])
     above_cube_pos = target_cube_pos + np.array([0, 0, 0.5])
 
+    gripper_id = model.actuator("actuator8").id
+    gripper_range = model.actuator(gripper_id).ctrlrange
+
+    gripper_close = gripper_range[0]
+    gripper_open = gripper_range[1]
+
     if exceeds_length:
-        close_cube_pos = target_cube_pos + np.array([0.01, 0, 0.08])
+        close_cube_pos = target_cube_pos + near_cube
     else:
-        close_cube_pos = target_cube_pos + np.array([0.01, -0.015, 0.08])
+        close_cube_pos = target_cube_pos + near_cube
 
 
     lift_cube_pos = start_pos + np.array([0, 0, 0.6])
 
     target_space_pos = data.xpos[space_id].copy()
-    above_target_pos = target_space_pos + np.array([0, 0, 0.5])
-    close_target_pos = target_space_pos + np.array([0, 0, 0.09])
+    above_target_pos = target_space_pos + np.array([0, 0, 0.07])
+    close_target_pos = target_space_pos + np.array([0, 0, 0.06])
 
     next_state = state
     goal_position = ee_pos.copy()
@@ -38,7 +46,7 @@ def pick_and_place(model,data, exceeds_length,gripper_id, space_id,cube_id, ee_p
         # move gripper to above the cube
         goal_position = above_cube_pos
         # change the state to open the gripper
-        if reached(ee_pos,above_cube_pos, tol):
+        if reached(ee_pos,goal_position, tol):
             next_state = "open_gripper"
         
     #open gripper    
@@ -50,45 +58,64 @@ def pick_and_place(model,data, exceeds_length,gripper_id, space_id,cube_id, ee_p
     
     elif state == "descend_to_cube":
         goal_position = close_cube_pos
-        if reached(ee_pos, close_cube_pos,tol=0.04):
+        if reached(ee_pos, goal_position,tol=0.05):
             next_state = "close_gripper"
-        
-    elif state == "close_gripper":
-        
-        data.ctrl[gripper_id] = gripper_close
-        print("time:", data.time,
-      "state:", state,
-      "gripper ctrl:", data.ctrl[gripper_id])
 
+    elif state == "close_gripper":
+        data.ctrl[gripper_id] = gripper_close
+        goal_position = close_cube_pos
         
-        if data.time - state_start_time > 1:
+        if data.time - state_start_time > 2.0:
             next_state = "lift"
 
-    elif state == "lift":
+    elif state =="lift":
+        data.ctrl[gripper_id] = gripper_close
+        print(f"[lift] Time: {data.time:.2f}, Gripper ctrl: {data.ctrl[gripper_id]}, Finger joints: {data.qpos[7:9]}")
         goal_position = lift_cube_pos
-        if reached(ee_pos,lift_cube_pos,tol):
-            next_state = "move_to_target"
+        if reached(ee_pos,goal_position,tol):
+            print("finish")
+
+
+
+            
+
+        
+    # elif state == "close_gripper":
+    #     goal_position == ee_pos.copy()
+    #     data.ctrl[gripper_id] = gripper_close
+    #     print("time:", data.time,
+    #   "state:", state,
+    #   "gripper ctrl:", data.ctrl[gripper_id])
+
+        
+    #     if data.time - state_start_time > 1:
+    #         next_state = "lift"
+
+    # elif state == "lift":
+    #     goal_position = lift_cube_pos
+    #     if reached(ee_pos,lift_cube_pos,tol):
+    #         next_state = "move_to_target"
     
-    elif state == "move_to_target":
-        goal_position = above_target_pos
-        if reached(ee_pos,above_target_pos,tol):
-            next_state = "descend_to_target"
+    # elif state == "move_to_target":
+    #     goal_position = above_target_pos
+    #     if reached(ee_pos,above_target_pos,tol):
+    #         next_state = "descend_to_target"
         
-    elif state == "descend_to_target":
-        goal_position = close_target_pos
-        if reached(ee_pos,close_target_pos,tol):
-            data.ctrl[gripper_id] = gripper_open
-            next_state = "move_up"
+    # elif state == "descend_to_target":
+    #     goal_position = close_target_pos
+    #     if reached(ee_pos,close_target_pos,tol):
+    #         data.ctrl[gripper_id] = gripper_open
+    #         next_state = "move_up"
         
-    elif state == "move_up":
-        goal_position = above_target_pos
-        if reached(ee_pos,above_target_pos,tol):
-            next_state = "default"
+    # elif state == "move_up":
+    #     goal_position = above_target_pos
+    #     if reached(ee_pos,above_target_pos,tol):
+    #         next_state = "default"
         
-    elif state == "default":
-        goal_position = start_pos + np.array([0,0,0.5])
-        if reached(ee_pos,start_pos + np.array([0,0,0.5]),tol):
-            next_state = "end"
+    # elif state == "default":
+    #     goal_position = start_pos + np.array([0,0,0.5])
+    #     if reached(ee_pos,start_pos + np.array([0,0,0.5]),tol):
+    #         next_state = "end"
 
 
     return next_state, goal_position